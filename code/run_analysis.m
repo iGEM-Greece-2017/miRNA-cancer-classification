bdd60d671a365188da6b998bd15d489681f85905
Konstantinos Samaras-Tsakiris <ksamtsak@gmail.com>
2017-03-18 01:54:53 +0200

    Add script to run all the analysis with 1 command

---
+++ code/run_analysis.m
@@ -0,0 +1,13 @@
+%% Find best miRNAs that cover the cancer cases sufficiently well
+[primtumor,normal,typnorm,reg]= preprocessGDC();
+
+params= struct(...
+    'totalcoverage',0.99,...    % Find enough miR to cover 99% of cancer cases
+    'coverageLim',0.1,...      % Accept miR with coverage >10%
+    'foldchange',10,...         % How many times greater/smaller in cancer than normal
+    'up_countLim',[10,68],...   % Read count lims. x: x<normlim, x>tumorlim
+    'down_countLim',[68,8]...   % Read count lims. x: x>normlim, x<tumorlim
+    );
+[bestCombos,bestCov]= solve_miR(primtumor,typnorm,reg, params);
+miRdata= utils.lookupSelectedCombos(bestCombos,bestCov,primtumor.Properties.RowNames,reg);
+miRdata
