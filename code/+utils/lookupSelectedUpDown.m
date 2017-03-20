a29833ffac5113943946a5acc95a5f8628db0b9a
Konstantinos Samaras-Tsakiris <ksamtsak@gmail.com>
2017-03-20 22:40:39 +0200

    Show significance in levels, not numbers

--- code/+utils/lookupSelectedUpDown.m
+++ code/+utils/lookupSelectedUpDown.m
@@ -6,18 +6,36 @@ upsat= comboSat{1}; downsat= comboSat{2};
 Upregulated_miR= all_miRnames(upmir);
 Downregulated_miR= all_miRnames(downmir);
 
-Significance_log10= round(log10(pvals(upmir)),3,'significant');
+Significance= cell(size(upmir,1),1);
+for i=1:size(upmir,1)
+  if pvals(upmir(i)) < 0.001
+    Significance{i}= '<0.001';
+  elseif pvals(upmir(i)) < 0.01
+    Significance{i}= '<0.01';
+  elseif pvals(upmir(i)) < 0.05
+    Significance{i}= '<0.05';
+  end
+end
 Coverage= round(sum(upsat(upmir,:),2)./size(upsat,2)*100,1);
 Inc_Coverage= round(comboCoverage{1}./size(upsat,2)*100,1);
 Mean_Fold_change= arrayfun(@(mir) exp(mean(regulation(mir,upsat(mir,:)),2)), upmir);
 Mean_Tumor_Reads_per_million= arrayfun(@(mir) mean(primtumor{mir,upsat(mir,:)},2), upmir);
 Mean_Normal_Reads_per_million= mean(normal{upmir,:},2);
-updata= table(Upregulated_miR,Significance_log10,Coverage,Inc_Coverage,Mean_Fold_change,Mean_Tumor_Reads_per_million,Mean_Normal_Reads_per_million);
+updata= table(Upregulated_miR,Significance,Coverage,Inc_Coverage,Mean_Fold_change,Mean_Tumor_Reads_per_million,Mean_Normal_Reads_per_million);
 
-Significance_log10= round(log10(pvals(downmir)),3,'significant');
+Significance= cell(size(downmir,1),1);
+for i=1:size(downmir,1)
+  if pvals(downmir(i)) < 0.001
+    Significance{i}= '<0.001';
+  elseif pvals(downmir(i)) < 0.01
+    Significance{i}= '<0.01';
+  elseif pvals(downmir(i)) < 0.05
+    Significance{i}= '<0.05';
+  end
+end
 Coverage= round(sum(downsat(downmir,:),2)./size(upsat,2)*100,3,'significant');
 Inc_Coverage= round(comboCoverage{2}./size(upsat,2)*100,3,'significant');
 Mean_Fold_change= arrayfun(@(mir) 1./exp(mean(regulation(mir,downsat(mir,:)),2)), downmir);
 Mean_Tumor_Reads_per_million= arrayfun(@(mir) mean(primtumor{mir,downsat(mir,:)},2), downmir);
 Mean_Normal_Reads_per_million= mean(normal{downmir,:},2);
-downdata= table(Downregulated_miR,Significance_log10,Coverage,Inc_Coverage,Mean_Fold_change,Mean_Tumor_Reads_per_million,Mean_Normal_Reads_per_million);
+downdata= table(Downregulated_miR,Significance,Coverage,Inc_Coverage,Mean_Fold_change,Mean_Tumor_Reads_per_million,Mean_Normal_Reads_per_million);
